8522edc1d3ebecf7897e854d3803d1a6c1bed92c
Andres Ruiz <andrsruiz@gmail.com>
2021-03-24 23:17:39 -0600

    Use config.json to use token and prefix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = '';
 
 client.once('ready', () => { console.log('Ready!'); });
 client.on('message', message => {
-    if(message.content == '!saymeme') message.channel.send('Hello there');
+	if(message.content == `${prefix}say`)
+		message.channel.send('Hello there');
 });
 
 client.login(token);
